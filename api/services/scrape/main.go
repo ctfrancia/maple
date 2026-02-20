bfb0d0fa67f3f00b839176a4888e9c7965ab6d48
Christian Francia <ctfrancia@gmail.com>
2026-02-20 17:55:34 +0100

    wip

--- api/services/scrape/main.go
+++ api/services/scrape/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ctfrancia/maple/foundation/logger"
 	"github.com/ctfrancia/maple/foundation/otel"
+	"github.com/joho/godotenv"
 )
 
 var build = "develop"
@@ -24,4 +25,21 @@ func main() {
 	}
 
 	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "SCRAPER", traceIDFn, events)
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "Error running scraper: %v", err)
+	}
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+	env := os.Getenv("SCRAPER_ENV")
+	if env == "" || env == "development" {
+		if err := godotenv.Load("zoltan/compose/.env.dev"); err != nil {
+			log.Info(ctx, "startup", "warn", "Warning: .env.dev file not found")
+		}
+	}
+
+	return nil
 }
