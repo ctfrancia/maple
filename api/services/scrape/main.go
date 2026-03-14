89a1decb4694a6513a3f097fad07e78a153f1b99
Christian Francia <ctfrancia@gmail.com>
2026-03-14 12:27:23 +0100

    wip

--- api/services/scrape/main.go
+++ api/services/scrape/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
-	//"time"
+	"time"
 
+	"github.com/ardanlabs/conf/v3"
 	"github.com/ctfrancia/maple/foundation/logger"
 	"github.com/ctfrancia/maple/foundation/otel"
 	"github.com/joho/godotenv"
-	//"github.com/ardanlabs/conf/v3"
 )
 
 var build = "develop"
@@ -44,45 +46,56 @@ func run(ctx context.Context, log *logger.Logger) error {
 	}
 
 	//-------------- CONFIG --------------
-	/*
-		cfg := struct {
-			conf.Version
-			Web struct {
-				ReadTimeout        time.Duration `conf:"default:5s"`
-				WriteTimeout       time.Duration `conf:"default:10s"`
-				IdleTimeout        time.Duration `conf:"default:120s"`
-				ShutdownTimeout    time.Duration `conf:"default:20s"`
-				APIHost            string        `conf:"default:0.0.0.0:3000"`
-				DebugHost          string        `conf:"default:0.0.0.0:3010"`
-				CORSAllowedOrigins []string      `conf:"default:*"`
-			}
-			Auth struct {
-				Host string `conf:"default:http://auth-service:6000"`
-			}
-			DB struct {
-				User         string `conf:"default:postgres"`
-				Password     string `conf:"default:postgres,mask"`
-				Host         string `conf:"default:database-service"`
-				Name         string `conf:"default:postgres"`
-				MaxIdleConns int    `conf:"default:0"`
-				MaxOpenConns int    `conf:"default:0"`
-				DisableTLS   bool   `conf:"default:true"`
-			}
-			Tempo struct {
-				Host        string  `conf:"default:tempo:4317"`
-				ServiceName string  `conf:"default:maple"`
-				Probability float64 `conf:"default:0.5"`
-				// Shouldn't use a high Probability value in non-developer systems.
-				// 0.05 should be enough for most systems. Some might want to have
-				// this even lower.
-			}
-		}{
-			Version: conf.Version{
-				Build: build,
-				Desc:  "Maple",
-			},
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout        time.Duration `conf:"default:5s"`
+			WriteTimeout       time.Duration `conf:"default:10s"`
+			IdleTimeout        time.Duration `conf:"default:120s"`
+			ShutdownTimeout    time.Duration `conf:"default:20s"`
+			APIHost            string        `conf:"default:0.0.0.0:8080"`
+			DebugHost          string        `conf:"default:0.0.0.0:8081"`
+			CORSAllowedOrigins []string      `conf:"default:*"`
+			UserAgent          string        `conf:"default:Mozilla/5.0 (compatible; MapleBot/1.0; +https://maple.chess)"`
+			ReqDelay           time.Duration `conf:"default:2s"`
 		}
-	*/
+		Auth struct {
+			Host string `conf:"default:http://auth-service:6000"`
+		}
+		DB struct {
+			User         string `conf:"default:postgres"`
+			Password     string `conf:"default:postgres,mask"`
+			Host         string `conf:"default:database-service"`
+			Name         string `conf:"default:postgres"`
+			MaxIdleConns int    `conf:"default:0"`
+			MaxOpenConns int    `conf:"default:0"`
+			DisableTLS   bool   `conf:"default:true"`
+		}
+		Tempo struct {
+			Host        string  `conf:"default:tempo:4317"`
+			ServiceName string  `conf:"default:maple"`
+			Probability float64 `conf:"default:0.5"`
+			// Shouldn't use a high Probability value in non-developer systems.
+			// 0.05 should be enough for most systems. Some might want to have
+			// this even lower.
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "Maple",
+		},
+	}
+
+	const prefix = "MAPLE"
+	help, err := conf.Parse(prefix, cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+
+		return fmt.Errorf("parsing config: %w", err)
+	}
 
 	return nil
 }
